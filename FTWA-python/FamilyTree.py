68ec656753f648e555b5fa9bba5242d0b2e11d70
YunusStahlschmidt <stahlschmidt.yunus@gmail.com>
2022-04-26 08:44:59 +0300

    added code for pretty printer

--- FTWA-python/FamilyTree.py
+++ FTWA-python/FamilyTree.py
@@ -1,3 +1,4 @@
+from FamilyMember import FamilyMember
 
 
 class FamilyTree(object):
@@ -5,11 +6,14 @@ class FamilyTree(object):
     def __init__(self, name):
         self.name = name
         self.members = []
+        self.root = None
 
-    def add_member(self, member):
+    def add_member(self, member: FamilyMember):
         self.members.append(member)
+        if len(self.members) == 0:
+            self.root = member
 
-    def update_member(self, member):
+    def update_member(self, member: FamilyMember):
         for i in range(len(self.members)):
             if self.members[i].name == member.name and self.members[i].surname == member.surname:
                 self.members[i] = member
--- FTWA-python/PrettyPrinter.py
+++ FTWA-python/PrettyPrinter.py
@@ -1,6 +1,52 @@
 
 
 class PrettyPrinter(object):
-
     def __init__(self):
         pass
+
+    def printNTree(self, x, flag, depth, isLast):
+        # Condition when node is None
+        if x == None:
+            return
+
+        # Loop to print the depths of the
+        # current node
+        for i in range(1, depth):
+            # Condition when the depth
+            # is exploring
+            if flag[i]:
+                print("| ", "", "", "", end="")
+
+            # Otherwise print
+            # the blank spaces
+            else:
+                print(" ", "", "", "", end="")
+
+        # Condition when the current
+        # node is the root node
+        if depth == 0:
+            if x.spouse is not None:
+                print(x.name + " " + x.surname + " & " +
+                      x.spouse.name + " " + x.spouse.surname)
+            else:
+                print(x.name + " " + x.surname)
+
+        # Condition when the node is
+        # the last node of
+        # the exploring depth
+        elif isLast:
+            print("+---", x.n)
+            # No more childrens turn it
+            # to the non-exploring depth
+            flag[depth] = False
+        else:
+            print("+---", x.n)
+
+        it = 0
+        for i in x.root:
+            it += 1
+
+            # Recursive call for the
+            # children nodes
+            self.printNTree(i, flag, depth + 1, it == (len(x.children) - 1))
+        flag[depth] = True
